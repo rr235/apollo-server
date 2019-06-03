b727bb5c0fa4b65ce61eaaa89594a33f80a81b91
Trevor Scheer <trevor@apollographql.com>
2019-06-03 14:20:29 -0700

    Write new tests to outline expected behavior

--- packages/apollo-federation/src/composition/validate/preComposition/__tests__/rootFieldUsed.test.ts
+++ packages/apollo-federation/src/composition/validate/preComposition/__tests__/rootFieldUsed.test.ts
@@ -93,4 +93,68 @@ describe('rootFieldUsed', () => {
       ]
     `);
   });
+
+  fit('warns against using default operation type names (Query, Mutation, Subscription) when a non-default operation type name is provided in the schema definition', () => {
+    const serviceA = {
+      typeDefs: gql`
+        schema {
+          query: RootQuery
+        }
+
+        type RootQuery {
+          product: Product
+        }
+
+        type Query {
+          invalidUseOfQuery: Boolean
+        }
+      `,
+      name: 'serviceA',
+    };
+
+    const warnings = validateRootFieldUsed(serviceA);
+
+    expect(warnings).toHaveLength(1);
+    expect(warnings).toMatchInlineSnapshot(`
+      Array [
+        [GraphQLError: [serviceA] Query -> Found invalid use of default root operation type \`Query\`. Default root operation type names (Query, Mutation, Subscription) are disallowed when a schema is defined or extended within a service.],
+      ]
+    `);
+  });
+
+  fit("doesn't warn against using default operation type names when no schema definition is provided", () => {
+    const serviceA = {
+      typeDefs: gql`
+        type Query {
+          validUseOfQuery: Boolean
+        }
+      `,
+      name: 'serviceA',
+    };
+
+    const warnings = validateRootFieldUsed(serviceA);
+    expect(warnings).toHaveLength(0);
+  });
+
+  fit("doesn't warn against using default operation type names when a schema is defined", () => {
+    const serviceA = {
+      typeDefs: gql`
+        schema {
+          mutation: Mutation
+        }
+
+        type Query {
+          validUseOfQuery: Boolean
+        }
+
+        type Mutation {
+          validUseOfMutation: Product
+        }
+      `,
+      name: 'serviceA',
+    };
+
+    const warnings = validateRootFieldUsed(serviceA);
+    expect(warnings).toHaveLength(0);
+  });
 });
